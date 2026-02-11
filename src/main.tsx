fb28bb64b601f306779901f962cc91662366d893
Bee <stockfisch97@gmail.com>
2026-02-11 18:07:12 +0100

    routing

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.PROD ? '/mtgdraft' : '/'}>
       <App />
     </BrowserRouter>
   </StrictMode>,
